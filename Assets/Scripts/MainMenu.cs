7164eafcc168f9e5cb0d5f52c428526c06b845c8
IanJeannin <36770934+IanJeannin@users.noreply.github.com>
2019-04-12 12:19:09 -0500

    Add basic Main Menu and Credits Page

---
+++ Assets/Scripts/MainMenu.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MainMenu : MonoBehaviour
+{
+    [SerializeField]
+    private string gameScene;
+    [SerializeField]
+    private string creditsScene;
+    [SerializeField]
+    private string mainMenu;
+
+    public void loadGameScene()
+    {
+        SceneManager.LoadScene(gameScene);
+    }
+
+    public void goToCredits()
+    {
+        SceneManager.LoadScene(creditsScene);
+    }
+
+    public void backToMenu()
+    {
+        SceneManager.LoadScene(mainMenu);
+    }
+
+    public void ExitGame()
+    {
+        Application.Quit();
+    }
+}
