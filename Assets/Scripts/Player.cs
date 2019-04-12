afa8fdbdd073f9df3b569f3a58a08d59b41630ec
IanJeannin <36770934+IanJeannin@users.noreply.github.com>
2019-04-12 13:46:45 -0500

    Add player script and create input for attacks
    Player script is meant to keep track of players health and their amount of charge, as well as possibly contain the attack actions.

---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerHealthAndCharge : MonoBehaviour
+{
+    [SerializeField]
+    private float maxHealth;
+    [SerializeField]
+    private float maxCharge;
+
+    private float currentHealth;
+    private float currentCharge;
+
+    private void Awake()
+    {
+        currentHealth = maxHealth;
+        currentCharge = 0;
+    }
+}
