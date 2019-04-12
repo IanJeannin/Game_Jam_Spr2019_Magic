a5230735ba49b59a3f445d4526921878ffd3fc34
DefinitelyNotAShark <definitelynotasharkohno@gmail.com>
2019-04-12 11:52:13 -0500

    Added player movement and clamped player position

---
+++ Assets/Scripts/KeepObjectOnScreen.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class KeepObjectOnScreen : MonoBehaviour
+{
+    [SerializeField]
+    private float yMaxPos;
+
+    private BoxCollider2D myColl;
+    private Vector3 onScreenPos;
+    private Vector3 bottomLeftWorldCoordinates;
+    private Vector3 topRightWorldCoordinates;
+    private Vector3 movementRangeMin;
+    private Vector3 movementRangeMax;
+
+    private void Start()
+    {
+        myColl = GetComponent<BoxCollider2D>();
+
+        bottomLeftWorldCoordinates = Camera.main.ViewportToWorldPoint(Vector3.zero);//this is bottom left
+        topRightWorldCoordinates = Camera.main.ViewportToWorldPoint(new Vector3(1, 1, 0));//this is top right
+
+        movementRangeMin = bottomLeftWorldCoordinates + myColl.bounds.extents;//this is our minimum 
+        movementRangeMax = topRightWorldCoordinates - myColl.bounds.extents;//this is our maximum
+    }
+    void Update()
+    {
+        onScreenPos = transform.position;
+        onScreenPos.x = Mathf.Clamp(transform.position.x, movementRangeMin.x, movementRangeMax.x);
+        onScreenPos.y = Mathf.Clamp(transform.position.y, movementRangeMin.y, yMaxPos);
+        transform.Translate(onScreenPos - transform.position);
+    }
+}
+
---
+++ Assets/Scripts/MovePlayer.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MovePlayer : MonoBehaviour
+{
+    [SerializeField]
+    private float speed;
+
+    private string horizontalName, verticalName;
+    private float horizontalValue, verticalValue;
+
+    private void Start()
+    {
+        horizontalName = "Horizontal";
+        verticalName = "Vertical";
+    }
+    private void FixedUpdate()
+    {
+        Move();
+    }
+
+    void Update()
+    {
+        GetInput();
+    }
+
+    void GetInput()
+    {
+        horizontalValue = Input.GetAxis(horizontalName);
+        verticalValue = Input.GetAxis(verticalName);
+    }
+
+    void Move()
+    {
+        transform.Translate(horizontalValue * speed * Time.deltaTime, verticalValue * speed * Time.deltaTime, 0);
+    }
+}
