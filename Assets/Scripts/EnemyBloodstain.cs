156ce6016742964bdb6979bc27ac13ab22b504e5
Ja'ire <jv9ufxcy@yahoo.com>
2019-04-13 00:36:42 -0500

    Added a burst Particle that can be recolored and used for all damage effects
    Added a Randomizer sprite mask for a death splatter

---
+++ Assets/Scripts/EnemyBloodstain.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyBloodstain : MonoBehaviour
+{
+    private SpriteRenderer spriteRend;
+    [SerializeField]
+    private Sprite[] bloodSplatters;
+    // Start is called before the first frame update
+    void Start()
+    {
+        spriteRend = GetComponent<SpriteRenderer>();
+        int rand = Random.Range(0, bloodSplatters.Length);
+        spriteRend.sprite = bloodSplatters[rand];
+    }
+}
